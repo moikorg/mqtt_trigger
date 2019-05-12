c0d449ac5c534918b4be506707c618375112e245
Michael Maeder <mike@moik.org>
2019-05-12 11:29:03 +0200

    some comments

--- code/mqtt_trigger.py
+++ code/mqtt_trigger.py
@@ -5,20 +5,21 @@ import datetime
 import requests
 import os
 
+sonos_api_url = 'http://docker.moik.org:5005/'
 switcher = {
-                "FF6897": "ircode/0",
-                "FF30CF": "ircode/1",
-                "FF18E7": "ircode/2",
-                "FF7A85": "ircode/3",
-                "FF10EF": "ircode/4",
-                "FF38C7": "ircode/5",
-                "FF5AA5": "ircode/6",
-                "FF42BD": "ircode/7",
-                "FF4AB5": "ircode/8",
-                "FF52AD": "ircode/9",
-                "FFA857": "volUp",
-                "FFE01F": "volDown",
-                "FFC23D": "playpause",
+                "FF6897": "wohnzimmer/linein",	# button '0'
+                "FF30CF": "",	# button '1'
+                "FF18E7": "ircode/2",	# button '2'
+                "FF7A85": "ircode/3",	# button '3'
+                "FF10EF": "ircode/4",	# button '4'
+                "FF38C7": "ircode/5",	# button '5'
+                "FF5AA5": "wohnzimmer/favorite/181.FM%20The%20Point",	# button '6'
+                "FF42BD": "wohnzimmer/favorite/181.FM%20Chilled%20Out",	# button '7'
+                "FF4AB5": "",	# button '8'
+                "FF52AD": "wohnzimmer/favorite/Radio%20Swiss%20Pop",	# button '9'
+                "FFA857": "wohnzimmer/volume/+3",	# button '+'
+                "FFE01F": "wohnzimmer/volume/-3",	# button '-'
+                "FFC23D": "wohnzimmer/playpause",	# button 'Play/Pause'
 }
 
 def configSectionMap(config, section):
@@ -60,7 +61,8 @@ def callback_irdata(client, userdata, message):
 
         cmd = switcher.get(message_dict[1], "unknown")
         if cmd != "unknown":
-            cmd = "http://docker.moik.org:5000/"+cmd
+            cmd = sonos_api_url + cmd
+            print("Sending the following request: ", cmd)
             r = requests.get(cmd)
             if r.status_code != 200:
                 print("Error in communication with SonosAPI. HTTP Error code: ", r.status_code)
